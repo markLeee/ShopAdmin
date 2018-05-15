397ae88a3dab7672a92cd7a149af833e9ac427ea
mark-lee <lee_930927@163.com>
2018-05-15 16:44:56 +0800

    del cli useless code and add login ele

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// 加载路径中可以使用 @ 绝对路径
+// @ 是 src 目录的绝对路径
+// @ 后面的 / 别忘了
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [{
+    path: '/login',
+    component: Login
+  }]
 })
